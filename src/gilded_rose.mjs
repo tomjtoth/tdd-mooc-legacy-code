b6258151d997c8cb769978b23eacc037665a1068
Tamás Tóth <tomjtoth@gmail.com>
2025-03-17 16:12:35 +0200

    refactored `Shop.updateQuality` via feature toggle defaulting to original method

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -23,32 +23,55 @@ export class Shop {
     this.items = items;
   }
 
-  updateQuality() {
+  updateQuality(useSwitch = false) {
     for (const item of this.items) {
-      if (!["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"].includes(item.name)) {
-        if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") item.quality--;
-      } else {
-        if (item.quality < 50) {
-          item.quality++;
+      if (useSwitch) {
+        switch (item.name) {
+          case "Aged Brie":
+            item.incQuality(--item.sellIn < 0 ? 2 : 1);
+            break;
 
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11 && item.quality < 50) item.quality++;
-            if (item.sellIn < 6 && item.quality < 50) item.quality++;
+          case "Backstage passes to a TAFKAL80ETC concert":
+            const expiry = item.sellIn--;
+
+            if (item.sellIn < 0) item.quality = 0;
+            else if (expiry < 6) item.incQuality(3);
+            else if (expiry < 11) item.incQuality(2);
+            else item.incQuality(1);
+            break;
+
+          case "Sulfuras, Hand of Ragnaros":
+            break;
+
+          default:
+            item.decQuality(--item.sellIn < 0 ? 2 : 1);
+        }
+      } else {
+        if (!["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"].includes(item.name)) {
+          if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") item.quality--;
+        } else {
+          if (item.quality < 50) {
+            item.quality++;
+
+            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+              if (item.sellIn < 11 && item.quality < 50) item.quality++;
+              if (item.sellIn < 6 && item.quality < 50) item.quality++;
+            }
           }
         }
-      }
 
-      if (item.name != "Sulfuras, Hand of Ragnaros") item.sellIn--;
+        if (item.name != "Sulfuras, Hand of Ragnaros") item.sellIn--;
 
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") item.quality--;
+        if (item.sellIn < 0) {
+          if (item.name != "Aged Brie") {
+            if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+              if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") item.quality--;
+            } else {
+              item.quality = 0;
+            }
           } else {
-            item.quality = 0;
+            if (item.quality < 50) item.quality++;
           }
-        } else {
-          if (item.quality < 50) item.quality++;
         }
       }
     }
